91ad9a3309eedcc8eacdee3f7696581a2d235303
dave <daveit058@gmail.com>
2024-06-24 17:36:28 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HGMCA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
